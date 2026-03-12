4359d2e97aad6d83112f2c142eef99763ab87a6f
MANYONGA KUIYAMBA REMY <24mk218si@udbl.ac.cd>
2026-03-12 15:02:07 +0100

    Création du header et de la section hero

---
+++ void main(){.dart
@@ -0,0 +1,47 @@
+void main(){
+  //String NOM ="Remy";
+  //int age=20
+  //double taille=19.5;
+  //bool majeur= true;
+  //print("je m'appelle $NOM, j'ai $age, je mesure $taille, je suis $majeur")
+
+
+//}
+
+//void main(){
+// """ int age = 19;
+  //double taille = 1.80;
+  //String nom = "Richard";
+  //bool majeur = true;
+  //print("je m'appelle ${nom} et j'ai ${19} ans et ${taille} est ma ma taille");
+
+//age >=18? print("vrai") : print("faux");
+//taille >= 1.60? print("vous etes majeur"): print("vous etes petit");
+
+  //List<int> moyenne = [9, 5, 6, 7, 8];
+  //int somme = 0;
+  //print(moyenne[0]);"""
+
+int a = 0;
+int b = 0;
+somme (int a, int b){
+  return a+b;
+}
+multiplication(int a, int b){
+  return a*b;
+}
+
+division(int a, int b){
+return a/b;
+}
+soustraction(int a, int b){
+  return a-b;
+}
+print(somme(5,6));
+
+}
+
+
+
+
+
